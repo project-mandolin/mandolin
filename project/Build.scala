3d8d239d8725ede42591892da225d564608649a8
Ben Wellner <wellner@mitre.org>
2016-10-14 11:49:31 -0400

    Initial branch for model selection

--- project/Build.scala
+++ project/Build.scala
@@ -97,7 +97,9 @@ object MandolinBuild extends Build {
       libraryDependencies ++= Seq(
       "org.apache.spark" %% "spark-core" % "2.0.0",
       "org.apache.spark" %% "spark-sql"  % "2.0.0",
-      "org.apache.spark" %% "spark-mllib"  % "2.0.0"      
+      "org.apache.spark" %% "spark-mllib"  % "2.0.0",
+      "com.typesafe.akka" %% "akka-actor" % "2.4.11",
+      "com.typesafe.akka" %% "akka-agent" % "2.4.11"
       )
     )
   }  
