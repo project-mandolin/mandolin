52b7e6b3b7ec81243b0a55f4f6a3360d48f4cf7c
Josh <jkraunelis@mitre.org>
2017-06-06 17:16:03 -0400

    updated configs to use random features

--- examples/weburl/run-mandolin-dist.sh
+++ examples/weburl/run-mandolin-dist.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-spark-submit --class org.mitre.mandolin.glp.spark.Mandolin ../../dist/mandolin-spark-0.3.5.jar --conf url.distributed.conf
+spark-submit --driver-memory 40G --class org.mitre.mandolin.glp.spark.Mandolin ../../dist/mandolin-spark-0.3.5.jar --conf url.distributed.conf
--- examples/weburl/run-model-selection-dist.sh
+++ examples/weburl/run-model-selection-dist.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-spark-submit --driver-memory 42g --class org.mitre.mandolin.mselect.SparkModelSelectionDriver ../../dist/mandolin-spark-0.3.5.jar --conf url.model-selection.distributed.conf
+spark-submit --driver-memory 56G --class org.mitre.mandolin.mselect.SparkModelSelectionDriver ../../dist/mandolin-spark-0.3.5.jar --conf url.model-selection.distributed.conf
